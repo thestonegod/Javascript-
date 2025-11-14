0e6414646fe601801fa5dbc4e0d23ba0acb82560
thestonegod <elshaddaisandstone@gmail.com>
2025-11-14 14:25:45 -0800

    Values & variables

--- external.js
+++ external.js
@@ -1,3 +1,14 @@
 alert ("HI people ");
 console.log ("Hello Maxine");
 console.log ( 10 + 10 );
+//A variable is an abstract location paired with a 
+// symbolic name that holds data.
+// Variable declaration creates a storage location inside the memory
+
+//1 Assigning values at the time of declaring a variable
+let username = ("Stinger")
+console.log (username)
+
+//Assigning values after a variable has been declared
+let username;
+username = "plyper"
