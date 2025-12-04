586528a8914d9b5041b63c7d9f289c165b51473d
thestonegod <elshaddaisandstone@gmail.com>
2025-12-04 15:08:18 -0800

    Learning & recalling

---
+++ recall.js
@@ -0,0 +1,68 @@
+document.write("El-Shaddai did this")
+alert("Hi,welcome")
+//Keywords to declare variables/ identifier
+//Var is the oldest keyword
+var lady=50;
+
+//Newest keyword introduced in ES6
+let $dot= 20
+if($dot >=15) {
+    let _fisher = 5;
+    console.log(_fisher);
+}
+
+
+//Constant variables
+//Variables with const can not be reassigned to new values
+const wildLife = "Animals";
+console.log(wildLife) ;
+
+
+
+//Scope
+//Global scope: Variables & functions declared in global scope are visible from anywhere in the program
+var _study = "3 girls";
+function example(){
+    console.log(_study);
+}
+example();
+
+//Function scope
+//It is created when a function is declared. Variables within the functon scope aren't visible outside of it
+//Note: Be sure to include semicolons after declaring funtions
+//Note remember to use quotes around string values 
+function menu(){
+    var list = "Banana, Milk, Butter, Fruit juice";
+    console.log(list);
+}
+menu();
+
+
+//Block scope: The visibility of variables and functions within a block of code
+//A block of code is a group of statements that are enclosed in curly braces ({})
+
+function blockScope(){
+    if (true){
+        let _lesson = "JavaScript";
+        console.log(_lesson);
+    }
+}
+blockScope();
+
+//Number
+//String: Storing and manipulating text
+let firstName = "Anna"
+let lastName = 'Boakye'
+console.log(firstName,lastName)
+
+//Number: It represents integers & floating point numbers
+let num = '56.83'
+console.log(typeof num)
+
+//Boolean
+let adapt = true
+let suffering = false
+console.log(adapt)
+
+let girlCoders = 20<70
+console.log(girlCoders)
